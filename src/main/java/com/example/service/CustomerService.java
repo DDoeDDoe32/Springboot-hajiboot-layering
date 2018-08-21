76aefadb215bff3afcf5c69eb18d56c3a2c76d10
DdoeDdoe32 <DdoeDdoe32@naver.com>
2018-08-21 22:09:28 +0900

    Springboot-hajiboot-layering

---
+++ src/main/java/com/example/service/CustomerService.java
@@ -0,0 +1,24 @@
+package com.example.service;
+
+import com.example.domain.Customer;
+import com.example.repository.CustomerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class CustomerService {
+	@Autowired
+	CustomerRepository customerRepository;
+
+	public Customer save(Customer customer) {
+		return customerRepository.save(customer);
+	}
+
+	public List<Customer> findAll() {
+		return customerRepository.findAll();
+	}
+
+	// 그 외의 메서드는 3장에서 정의합니다.
+}
